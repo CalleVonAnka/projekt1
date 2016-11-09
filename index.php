fb77e02c88eb1357b411af2815d72bd1ebf69848
Carl Philip <carlphilip93@hotmail.com>
2016-11-09 13:57:24 +0100

    lagt till filer
    med form

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>myCloset</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="loginForm">
+      <form action="index.php" method="post" enctype="multipart/form-data">
+        <input placeholder="Enter username" name="username" type="text" autofocus>
+        <input placeholder="Enter password" name="password" type="password">
+        <br/>
+        <input name="login" type="submit" value="Login" class="btn-login">
+        <input name="register" type="submit" value="Register" class="btn-register">
+      </form>
+    </div>
+  </body>
+</html>
